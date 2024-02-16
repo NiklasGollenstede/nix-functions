c6e8b3486e61bd253a83c0c6da7b078dcde40f9c
Niklas Gollenstede <niklas.gollenstede@web.de>
2024-02-16 12:04:27 +0100

    substituteImplicit: expose internal variables

--- lib/scripts.nix
+++ lib/scripts.nix
@@ -12,16 +12,29 @@ in rec {
         )) attrs)
     );
 
-    # This function allows using nix values in bash scripts, without the need to pass an explicit and manually curated list of values to the script.
-    # Given a path list of bash script »sources« and an attrset »context«, this function parses the scripts for the literal sequence »@{« followed by a lookup path of period-joined words, and for each map resolves the attribute path against »context«, declares a variable with that value, and swaps out the »@{« plus path for a »${« use of the declared variable. The returned script sources the variable definitions and all translated »sources« in order.
-    # The lookup path may end in »!« plus the name of a function and optionally string arguments separated by ».«s, in which case the function is taken from »helpers//inputs.self.lib//(pkgs.lib or lib)//pkgs//builtins« and called with the string args and the resolved value as last arg; the return value then replaces the resolved value. Examples: »!attrNames«, »!toJSON«, »!catAttrs«, »!hashString.sha256«, »!writeText.filename«.
+    # This function allows using nix values in bash scripts, without the need to pass an explicit and manually curated list of values to the script:
+    # Given a path list of bash script »sources« and an attrset »context«, this function parses the scripts for the literal sequence »@{« followed by a lookup path of period-joined words, and, for each match, resolves the attribute path against »context«, declares a variable with that value, and swaps out the »@{« plus path for a »${« use of the declared variable.
+    # The returned script sources the variable definitions and all translated »sources« in order.
+    #
+    # The lookup path may end in »!« plus the name of a function and optionally string arguments separated by ».«s, in which case the function is taken from »helpers//inputs.self.lib//(pkgs.lib or lib)//pkgs//builtins« and called with the string args and the resolved value as last arg; the return value then replaces the resolved value.
+    # Examples: »!attrNames«, »!toJSON«, »!catAttrs«, »!hashString.sha256«, »!writeText.filename«.
+    #
     # The names of the declared values are the lookup paths, with ».«/»!«/»-« replaced by »_«/»1«/»0«.
     # The symbol immediately following the lookup path (/builtin name) can be »}« or any other symbol that bash variable substitutions allow after the variable name (like »:«, »/«), eliminating the need to assign to a local variable to do things like replacements, fallbacks or substrings.
+    #
     # If the lookup path does not exist in »context«, then the value will be considered the same as »null«, and a value of »null« will result in a bash variable that is not defined (which can then be handled in the bash script).
     # Other scalars (bool, float, int, path) will be passed to »builtins.toString«. Anything that has an ».outPath« that is a string will be passed as that ».outPath«.
+    #
     # Lists will be declared as bash arrays, attribute sets will be declared as associative arrays using »asBashDict«.
     # Bash does not support any nested data structures. Lists or attrsets within lists or attrsets are therefore (recursively) encoded and escaped as strings, such that calling »eval« on them is safe if (but only if) they are known to be encoded from nested lists/attrsets. Example: »eval 'declare -A fs='"@{config.fileSystems['/']}" ; root=${fs[device]}«.
-    # Any other value (functions), and things that »builtins.toString« doesn't like, will throw here.
+    #
+    # Any other value type (functions), and things that »builtins.toString« doesn't like, or things that fail to evaluate, will throw here.
+    #
+    # This function returns an attribute set that can be cast to a (script) string, for the full effect described above, but additionally exposes the following attributes:
+    # * »decls«: deduplicated list of the lookup paths (as verbatim strings) of all encountered »@{« substitutions,
+    # * »vars«: attribute set mapping each »decls« entry to its resolved value,
+    # * »bash-ify«: function with arguments »decl« and »value« that creates a bash variable declaration (with value assignment),
+    # * »scripts«: the processed scripts sourced by main script returned.
     substituteImplicit = args@{
         scripts, # List of paths to scripts to process and then source in the returned script. Each script may also be an attrset »{ name; text; }« instead of a path.
         context, # The root attrset for the resolution of substitutions.
@@ -29,12 +42,12 @@ in rec {
         helpers ? { }, # Attrset of (highest priority) helper functions.
         trace ? (m: v: v), # Function that gets called with the names and values as they are processed. Pass »builtins.trace« for debugging, esp. when evaluating one of the accessed values fails.
     }: let
-        scripts = map (source: rec {
+        parsedScripts = map (source: rec {
             text = if builtins.isAttrs source then source.text else builtins.readFile source; name = if builtins.isAttrs source then source.name else builtins.baseNameOf source;
             parsed = builtins.split ''@\{([#!]?)([a-zA-Z][a-zA-Z0-9_.-]*[a-zA-Z0-9](![a-zA-Z][a-zA-Z0-9_.-]*[a-zA-Z0-9])?)([:*@\[#%/^,\}])'' text; # (first part of a bash parameter expansion, with »@« instead of »$«)
         }) args.scripts;
-        decls = lib.unique (map (match: builtins.elemAt match 1) (builtins.filter builtins.isList (builtins.concatMap (script: script.parsed) scripts)));
-        vars = pkgs.writeText "vars" ("#!/usr/bin/env bash\n" + (builtins.concatStringsSep "\n" (map (decl: let
+        decls = lib.unique (map (match: builtins.elemAt match 1) (builtins.filter builtins.isList (builtins.concatMap (script: script.parsed) parsedScripts)));
+        vars = builtins.listToAttrs (map (decl: let
             call = let split = builtins.split "!" decl; in if (builtins.length split) == 1 then null else builtins.elemAt split 2;
             path = (builtins.filter builtins.isString (builtins.split "[.]" (if call == null then decl else builtins.substring 0 ((builtins.stringLength decl) - (builtins.stringLength call) - 1) decl)));
             resolved = lib.attrByPath path null context;
@@ -42,6 +55,8 @@ in rec {
                 split = builtins.filter builtins.isString (builtins.split "[.]" call); name = builtins.head split; args = builtins.tail split;
                 func = builtins.foldl' (func: arg: func arg) (helpers.${name} or self.lib.${name} or (pkgs.lib or lib).${name} or pkgs.${name} or builtins.${name}) args;
             in func resolved);
+        in { name = decl; value = applied; }) decls);
+        bash-ify = decl: applied: let
             value = if builtins.isString (applied.outPath or null) then applied.outPath else if (
                 (builtins.isBool applied) || (builtins.isFloat applied) || (builtins.isInt applied) || (builtins.isPath applied)
             ) then builtins.toString applied else applied;
@@ -59,15 +74,18 @@ in rec {
             else if (builtins.isList value) then "${name}=${toStringRecursive value}"
             else if (builtins.isAttrs value) then "declare -A ${name}=${toStringRecursive value}"
             else throw "Can't use value of unsupported type ${builtins.typeOf} as substitution for ${decl}" # builtins.isFunction
-        ); in trace final final)) decls)));
-    in ''
-        source ${vars}
-        ${builtins.concatStringsSep "\n" (map (script: "source ${pkgs.writeScript script.name (
+        ); in trace final final);
+        declarations = pkgs.writeText "vars" ("#!/usr/bin/env bash\n" + (builtins.concatStringsSep "\n" (lib.mapAttrsToList (bash-ify) vars)));
+        processedScripts = map (script: pkgs.writeScript script.name (
             builtins.concatStringsSep "" (map (seg: if builtins.isString seg then seg else (
                 "$"+"{"+(builtins.head seg)+(builtins.replaceStrings [ "." "!" "-" ] [ "_" "1" "0" ] (builtins.elemAt seg 1))+(toString (builtins.elemAt seg 3))
             )) script.parsed)
-        )}") scripts)}
-    '';
+        )) parsedScripts;
+        script = ''
+            source ${declarations}
+            ${builtins.concatStringsSep "\n" (map (script: "source ${script}") processedScripts)}
+        '';
+    in { __toString = _: script; inherit script decls vars bash-ify; scripts = [ declarations ] ++ processedScripts; };
 
     ## Given a bash »script« as string and a function »name«, this finds and extracts the definition of that function in and from the script.
     #  The function definition has to start at the beginning of a line and must end on the next line that is a sole »}« or »)}«.
